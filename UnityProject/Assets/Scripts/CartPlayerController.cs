3f7871f398b5123ad1c988f63125b99af39b82e8
Luca Nixdorf <nixdorf@uni-bremen.de>
2018-01-26 18:22:41 +0100

    Added a player controller script for the cart player

---
+++ UnityProject/Assets/Scripts/CartPlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CartPlayerController : MonoBehaviour
+{
+    public GameObject lamp;
+
+    public ParticleSystem breaksEffect;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            /*if (!breaksEffect.isPlaying)
+            {
+            }*/
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
