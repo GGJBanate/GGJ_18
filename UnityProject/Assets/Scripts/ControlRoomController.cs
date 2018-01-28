7c942196688bd6ef9c30c10d473077b8cf9d2d23
Jeffrey Bissel <bissel@uni-bremen.de>
2018-01-28 13:31:40 +0100

    blub

--- UnityProject/Assets/Scripts/ControlRoomController.cs
+++ UnityProject/Assets/Scripts/ControlRoomController.cs
@@ -1,21 +1,70 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.UI;
 
 public class ControlRoomController : MonoBehaviour
 {
     public TrackData track;
     public Dictionary<Pos, TrackPieceData> map;
 
+
+	public GameObject intro;
+	private Text introText;
+	private Text pressEnter;
+	private Text waitingMessage;
+	private Text readyMessage;
+
+	private bool started;
     private TrackMapDisplay mapScreen;
+
+    private LocalPlayerNetworkConnection localPlayer;
 
     public void Init()
     {
     	mapScreen = GameObject.Find("MapScreen").GetComponent<TrackMapDisplay>();
     	mapScreen.init(map);
     }
+	public void Start() {
+        localPlayer = FindObjectsOfType<LocalPlayerNetworkConnection>().First(l => l.isLocalPlayer);
 
-    void Update () {
-        mapScreen.map = this.map;
-    }
+        intro = Instantiate (intro);
+		intro.transform.SetAsLastSibling ();
+
+		Text[] introTexts = intro.GetComponentsInChildren<Text> ();
+		introText = introTexts[0];
+		pressEnter = introTexts [1];
+		waitingMessage = introTexts [2];
+		readyMessage = introTexts [3];
+		readyMessage.gameObject.SetActive (false);
+	}
+
+	public void Update() {
+		if (GameServer.Instance.gameStatus == GameStatus.Waiting) {
+			if (Input.GetButtonDown ("Submit")) {
+				LocalPlayerNetworkConnection connectionObj = FindObjectsOfType<LocalPlayerNetworkConnection> ().First (l => l.isLocalPlayer);
+				connectionObj.StartGame ();
+				readyMessage.gameObject.SetActive (true);
+				introText.gameObject.SetActive (false);
+				pressEnter.gameObject.SetActive (false);
+			}
+			waitingMessage.gameObject.SetActive (true);
+
+			return;
+		} else if (!started) {
+			waitingMessage.gameObject.SetActive(false);
+			intro.SetActive (false);
+			started = true;
+		}
+	    if (GameServer.Instance.gameStatus == GameStatus.Ongoing)
+        {
+            if (Input.GetButtonDown("BarrierSwitch"))
+            {
+                Debug.Log("Setting Barrier");
+                localPlayer.SetBarrier();
+            }
+		}
+		mapScreen.map = this.map;
+	}
 }
